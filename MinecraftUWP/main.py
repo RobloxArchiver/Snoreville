d207c02ac0275ecda5d76d18f35c2d1961808249
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-01-20 16:05:35 -0500

    wefwefwefwef

--- MinecraftUWP/main.py
+++ MinecraftUWP/main.py
@@ -1,3 +1,29 @@
-import pymem
+#-------------------------------------------------------------------------------
+# Credits:
+#
+#     Snaacky @ GitHub (https://github.com/Snaacky) // Used a bit of code from mw2tweaks
+#-------------------------------------------------------------------------------
 
-pm = pymem.Pymem("Minecraft.Windows.exe")
+from os import system
+from pymem import exception, Pymem
+
+system("title MCBE Module Dumper")
+
+try:
+    minecraft_process = Pymem("Minecraft.Windows.exe")
+except exception.ProcessNotFound:
+    print("Please open Minecraft first")
+    input("Press 'ENTER' to exit!")
+
+def main():
+    print("Minecraft Process Grabbed...\nDumping...\n\n")
+
+    modules = list(minecraft_process.list_modules())
+
+    for module in modules:
+        print(f"{module.name}: {module.filename}")
+
+    input("\nDump Finished! Press 'ENTER' to exit!")
+
+if __name__ == "__main__":
+    main()
